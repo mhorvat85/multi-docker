84d28d3e4244a71998e784f0ebdfcda6292c5ed7
Marko Horvat <horvat.marko2212@gmail.com>
2024-04-08 04:27:40 +0200

    testing deployment3

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const OtherPage = () => {
   return (
     <div>
-      In some other page!
+      In some other page which is blank!
       <Link to="/">Go back home</Link>
     </div>
   );
